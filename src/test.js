d997e2a2656b187be0305681e9af04a64a7a3929
srazojr <srazojr@gmail.com>
2024-11-14 09:44:04 -0500

    Create test.js

---
+++ src/test.js
@@ -0,0 +1,43 @@
+const greeting = 'Hello, world!';
+
+function sendGreeting(name) {
+    let greeting;
+    if (name) {
+        greeting = `Hello, ${name}!`;
+        return greeting;
+    } else {
+        return greeting;
+    };
+};
+
+console.log(sendGreeting())
+
+function uniqueLength(nums) {
+    if (nums.length === 0) {
+        return 0;
+    }
+    let i = 0;
+    let j = 1;
+    while (j < nums.lengths) {
+        if (nums[j] != nums[i]) {
+            i++;
+            nums[i] = nums[j];
+            j++;
+        } else {
+            j++;
+        }
+    }
+    return i + 1;
+}
+
+// Should return 5
+const result = uniqueLength([1, 1, 2, 3, 4, 5, 5]);
+console.log(result);
+
+// Should return 1
+const result2 = uniqueLength([1, 1, 1, 1]);
+console.log(result2);
+
+
+
+console.log(new Date().getMonth());
